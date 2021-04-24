bb9e73072c4472df2f7491a70b21962c26243a64
SILING LI <839952091@qq.com>
2021-04-23 23:02:25 -0400

    Add files via upload

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -1,12 +1,14 @@
 package lab9;
 
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Queue;
 import java.util.Set;
 
 /**
  * Implementation of interface Map61B with BST as core data structure.
  *
- * @author Your name here
+ * @author Siling Li
  */
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
@@ -44,7 +46,13 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      *  or null if this map contains no mapping for the key.
      */
     private V getHelper(K key, Node p) {
-        throw new UnsupportedOperationException();
+        // Return value associated with key in the subtree rooted at p;
+        // return null if key not present in subtree rooted at p.
+        if (p == null) return null;
+        int cmp = key.compareTo(p.key);
+        if (cmp < 0) return getHelper(key, p.left);
+        else if (cmp > 0) return getHelper(key, p.right);
+        else return p.value;
     }
 
     /** Returns the value to which the specified key is mapped, or null if this
@@ -52,14 +60,25 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return getHelper(key, root);
     }
 
     /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
       * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
      */
     private Node putHelper(K key, V value, Node p) {
-        throw new UnsupportedOperationException();
+        // Change key’s value to value if key in subtree rooted at p.
+        // Otherwise, add new node to subtree associating key with value.
+        if (p == null) return new Node(key, value);
+        int cmp = key.compareTo(p.key);
+        if (cmp < 0) {
+            p.left = putHelper(key, value, p.left);
+        } else if (cmp > 0) {
+            p.right = putHelper(key, value, p.right);
+        } else {
+            p.value = value;
+        }
+        return p;
     }
 
     /** Inserts the key KEY
@@ -67,21 +86,55 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        // Search for key. Update value if found; grow table if new.
+        root = putHelper(key, value, root);
+        size += 1;
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
+    private void keySetHelper(Set<K> keySet, Node node) {
+        if (node == null) {
+            return;
+        }
+        keySet.add(node.key);
+        keySetHelper(keySet, node.left);
+        keySetHelper(keySet, node.right);
+    }
 
     /* Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> keySet = new HashSet<>();
+        keySetHelper(keySet, root);
+        return keySet;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return keySet().iterator();
+    }
+
+    // min Key in the tree
+    public K min() {
+        return minHelper(root).key;
+    }
+    // min key rooted at node p
+    private Node minHelper(Node p) {
+        if (p.left == null) return p;
+        return minHelper(p.left);
+    }
+
+    private Node deleteMin(Node x)
+    {
+        if (x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        return x;
     }
 
     /** Removes KEY from the tree if present
@@ -90,7 +143,28 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        V returnValue = get(key);
+        if (returnValue == null) return null;
+        root = removeHelper(root, key);
+        size -= 1;
+        return returnValue;
+    }
+
+    private Node removeHelper(Node p, K key){
+        if (p == null) return null;
+        int cmp = key.compareTo(p.key);
+        if (cmp < 0) p.left = removeHelper(p.left, key);
+        else if (cmp > 0) p.right = removeHelper(p.right, key);
+        else
+        {
+            if (p.right == null) return p.left;
+            if (p.left == null) return p.right;
+            Node t = p;
+            p = minHelper(t.right); // See page 407.
+            p.right = deleteMin(t.right);
+            p.left = t.left;
+        }
+        return p;
     }
 
     /** Removes the key-value entry for the specified key only if it is
@@ -99,11 +173,11 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      **/
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        V returnValue = get(key);
+        if (returnValue == null | !returnValue.equals(value)) return null;
+        root = removeHelper(root, key);
+        size -= 1;
+        return returnValue;
     }
 
-    @Override
-    public Iterator<K> iterator() {
-        throw new UnsupportedOperationException();
-    }
 }
--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -1,5 +1,6 @@
 package lab9;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -7,22 +8,27 @@ import java.util.Set;
  *  A hash table-backed Map implementation. Provides amortized constant time
  *  access to elements via get(), remove(), and put() in the best case.
  *
- *  @author Your name here
+ *  @author Siling Li
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
 
-    private static final int DEFAULT_SIZE = 16;
+    private static final int DEFAULT_SIZE = 16; // init capacity and m
     private static final double MAX_LF = 0.75;
 
     private ArrayMap<K, V>[] buckets;
-    private int size;
+    private int size; //n
 
     private int loadFactor() {
         return size / buckets.length;
     }
 
-    public MyHashMap() {
+    public MyHashMap(){
         buckets = new ArrayMap[DEFAULT_SIZE];
+        this.clear();
+    }
+
+    public MyHashMap(int m) {
+        buckets = new ArrayMap[m];
         this.clear();
     }
 
@@ -30,8 +36,8 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     @Override
     public void clear() {
         this.size = 0;
-        for (int i = 0; i < this.buckets.length; i += 1) {
-            this.buckets[i] = new ArrayMap<>();
+        for (int i = 0; i < buckets.length; i += 1) {
+            buckets[i] = new ArrayMap<K, V>();
         }
     }
 
@@ -43,7 +49,6 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
         if (key == null) {
             return 0;
         }
-
         int numBuckets = buckets.length;
         return Math.floorMod(key.hashCode(), numBuckets);
     }
@@ -53,19 +58,37 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        if (key == null) throw new IllegalArgumentException("argument to get() is null");
+        int i = hash(key);
+        return buckets[i].get(key);
     }
+
 
     /* Associates the specified value with the specified key in this map. */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        // double table size if load factor >= MAX_LF
+        if (loadFactor()>=MAX_LF) resize(2*buckets.length);
+
+        int i = hash(key);
+        if (!buckets[i].containsKey(key)) size++;
+        buckets[i].put(key, value);
     }
 
+    private void resize(int chains) {
+        MyHashMap<K, V> temp = new MyHashMap<>(chains);
+        for (int i = 0; i < buckets.length; i++) {
+            for (K key : buckets[i].keySet()) {
+                temp.put(key, buckets[i].get(key));
+            }
+        }
+        this.size = temp.size;
+        this.buckets = temp.buckets;
+    }
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
@@ -81,7 +104,16 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * UnsupportedOperationException. */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        if (key == null) throw new IllegalArgumentException("argument to delete() is null");
+
+        int i = hash(key);
+        V returnValue = get(key);
+        if (buckets[i].containsKey(key)) size--;
+        buckets[i].remove(key);
+
+        // halve table size if average length of list <= 2
+        if (loadFactor()<MAX_LF) resize(buckets.length/2);
+        return returnValue;
     }
 
     /* Removes the entry for the specified key only if it is currently mapped to
@@ -89,11 +121,32 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * throw an UnsupportedOperationException.*/
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (key == null) throw new IllegalArgumentException("argument to delete() is null");
+
+        int i = hash(key);
+        V returnValue = get(key);
+        if (returnValue != value) return null;
+        if (buckets[i].get(key) == value) size--;
+        buckets[i].remove(key);
+
+        // halve table size if average length of list <= 2
+        if (loadFactor()<MAX_LF / 2) resize(buckets.length/2);
+        return returnValue;
     }
 
     @Override
     public Iterator<K> iterator() {
         throw new UnsupportedOperationException();
     }
+
+    // return keys in symbol table as an Iterable
+    public Iterable<K> keys() {
+        Set<K> queue = new HashSet<K>();
+        for (int i = 0; i < buckets.length; i++) {
+            for (K key : buckets[i])
+                queue.add(key);
+        }
+        return queue;
+    }
+
 }
